124ff9c2e0864a7d6014becf87c80436930d489e
we23256777 <63162705+we23256777@users.noreply.github.com>
2020-09-15 21:38:44 +0900

    Add files via upload

---
+++ 학번과 이름.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace 학번과_이름_출력1
+{
+# include <stdio.h>
+
+  
+
+    int main(void)
+
+    {
+
+        printf("%d \n", 201987068);
+
+        printf("박원정");
+
+
+
+        return 0;
+
+    }
+
+
+}
